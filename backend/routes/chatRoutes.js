7b43020eaf6bde29e30bb648697e13a2042f9fb6
Gopikrishnan Kanan <gopikrishnan.kanan@aadharhousing.com>
2024-01-30 18:26:39 +0530

    day2

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { accessChat, fetchChats } = require("../controllers/chatController");
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroup,
+} = require("../controllers/chatController");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
-// router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
-// router.route("/groupremove").post(protect, removeFromGroup);
+router.route("/group").post(protect, createGroupChat);
+router.route("/rename").put(protect, renameGroup);
 // router.route("/groupadd").post(protect, addToGroup);
+// router.route("/groupremove").post(protect, removeFromGroup);
 
 module.exports = router;
